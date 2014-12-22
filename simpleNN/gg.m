077fe79c63ca1fae8508378efb8731afefcd74ee
Hasse Tung <hasse.tung@gmail.com>
2014-12-23 00:02:46 +0800

    refine

--- simpleNN/gg.m
+++ simpleNN/gg.m
@@ -23,14 +23,16 @@ fprintf('Loading Data ...\n');
 M = csvread ('/Volumes/RamDisk/train.csv');
 N = csvread('/Volumes/RamDisk/test.csv');
 X = M(2:42001,2:785);
+X = rotateStd(X);
 y = M(2:42001,1);
 y(y==0) = 10;
 Xpred = N(2:28001,:);
+Xpred = rotateStd(Xpred);
 K = 100;
 
 fprintf('Doing principle components analysis ...\n');
 L = [X; Xpred];
-L = deepen(L, 194);
+%L = deepen(L, 194);
 Z = redu_dim(L, K);
 Zpred = Z((1 + size(X, 1)):end, :);
 Z = Z(1:size(X, 1), :);
@@ -49,23 +51,30 @@ Z = Z(sel(1:(tempN * .6)),:);
 y = y(sel(1:25200));
 clear L M N tempN sel
 fprintf('Initialization competed, go or press Ctrl+C into manual mode ...\n');
-pause;
+if ~exist('manualMode')
+	pause;
+end
 
 fprintf('Selecting lambda ...\n');
 layer_size = [K 500 500 length(unique(y))]
 initial_nn_params = initializeWeight(layer_size);
-lambdaVec = [(0:0.001:0.003) (0.004:0.003:0.01) (0.03:0.02:0.1) (0.1:0.3:1)]';
+%lambdaVec = [(0:0.001:0.003) (0.004:0.003:0.01) (0.03:0.02:0.1) (0.1:0.3:1)]';
 lambda = seLambda(initial_nn_params, layer_size, Z, y, Zval, yval, lambdaVec);
 fprintf('Lambda selection completed, continue or press Ctrl+C into manual mode ...\n');
-close all;
+if ~exist('manualMode')
+	pause;
+	close all;
+end
 
 fprintf('Training ...\n');
-nn_params = train(layer_size, Z, y, lambda, initial_nn_params, 300);
+nn_params = train(layer_size, Z, y, lambda, initial_nn_params, 500);
 test(nn_params, layer_size, Z, y);
 test(nn_params, layer_size, Zval, yval);
 test(nn_params, layer_size, Ztest, ytest);
 fprintf('Training completed, go onto manual mode or press Ctrl+C to quit.\n');
-pause;
+if ~exist('manualMode')
+	pause;
+end
 
 fprintf('Writing predicted results ...\n');
 pred = predict(nn_params, layer_size, Zpred);
