cf7b28b20e69f919e2bfa2673e28d85e3da8d4ef
Hasse Tung <hasse.tung@gmail.com>
2014-12-22 23:06:15 +0800

    rotate matrix in order to virtualize standardly

---
+++ simpleNN/rotateStd.m
@@ -0,0 +1,20 @@
+function scalarMatrix = rotateStd(scalarMatrix, n_row);
+	[m, n] = size(scalarMatrix);
+	if ~exist('n_row')
+		n_row = n_col = sqrt(n);
+	else
+		n_col = n / n_row;
+	end
+	for i = 1:m
+		scalarMatrix(i, :) = reshape(scalarMatrix(i, :), n_row, n_col)'(:);
+	end
+	%outputMatrix = zeros(numel(scalarMatrix),1);
+	%Matrix = reshape(scalarMatrix, n_row, n_col);
+	%[n_row n_col] = size(Matrix);
+	%for i = 1:n_row
+	%	to=(n_row+1-i)*n_col;
+	%	from=to-n_col+1;
+	%	outputMatrix(from:to)=Matrix(i,:);
+	%end
+	%outputMatrix = reshape(outputMatrix, n_col, n_row);
+end
